ce2c4f269e045fb7c5d5ded670277f99825a2395
Zakariye Ali <zakariyeali351@gmail.com>
2026-05-01 22:02:14 +0300

    python loops

--- js/index.js
+++ js/index.js
@@ -23,17 +23,7 @@ function fizzbuzzPrinter() {
   }
 }
 
-function fizzbuzz(num) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } else if (num % 3 === 0) {
-    return "Fizz";
-  } else if (num % 5 === 0) {
-    return "Buzz";
-  } else {
-    return num;
-  }
-}
+
 
 /*
   Write a method `reverse_string` that takes one argument, a string, and reverses
--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+    while counter > 0:
+        print(counter)
+        counter -= 1
 
-def fizzbuzz():
-    # code goes here!
-    pass
+    print("Happy New Year!")
+
+happy_new_year()
+    
+
+def square_integers(intlist):
+    squaredlist = []
+
+    for num in intlist:
+        squaredlist.append(num ** 2)
+
+    return squaredlist
+
+print(square_integers([1, 2, 3, 4, 5]))
+print(square_integers([-1, -2, -3, -4, -5]))
+
+
+def fizzbuzz(num=None):
+    # If no argument is provided, print 1 to 100
+    if num is None:
+        for i in range(1, 101):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+    else:
+        # Handle single number as before
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
+        elif num % 3 == 0:
+            return "Fizz"
+        elif num % 5 == 0:
+            return "Buzz"
+        else:
+            return num
+
+# This will print 1-100 as the test expects
+fizzbuzz()
